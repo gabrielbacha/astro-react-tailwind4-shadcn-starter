5a8e346ac8a89d61b6bfea1e90d5508cff439381
gabrielbacha <bachagabriel@gmail.com>
2025-04-14 19:45:16 +0400

    fixed base

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,8 @@ import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	site: import.meta.env.MODE === "production" ? "https://gabrielbacha.github.io/astro-react-tailwind4-shadcn-starter" : "http://localhost:4321",
+	site: import.meta.env.MODE === "production" ? "https://gabrielbacha.github.io/astro-react-tailwind4-shadcn-starter/" : "http://localhost:4321",
+	base: import.meta.env.MODE === "production" ? "/astro-react-tailwind4-shadcn-starter/" : "/",
 	integrations: [mdx(), react(), sitemap(), icon()],
 	vite: {
 		plugins: [tailwindcss()],
