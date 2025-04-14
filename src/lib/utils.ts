725fb1be430e2806ef9f8451adcf4bad9ab697e7
gabrielbacha <bachagabriel@gmail.com>
2025-04-14 21:49:46 +0400

    cleanups

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,19 +2,23 @@ import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function getAssetPath(path: string) {
+	return import.meta.env.BASE_URL + path;
 }
 
 export function formatDate(date: Date) {
-  return Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }).format(date)
+	return Intl.DateTimeFormat("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric"
+	}).format(date);
 }
 
 export function readingTime(html: string) {
-  const textOnly = html.replace(/<[^>]+>/g, '')
+	const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length
   const readingTimeMinutes = (wordCount / 200 + 1).toFixed()
   return `${readingTimeMinutes} min read`
